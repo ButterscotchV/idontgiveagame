34cc7013468f4e0b7cb0405332efec46f77b8f54
Paul Mauviel <p.a.mauviel@gmail.com>
2020-01-31 23:14:11 -0500

    Adding a couple of words into a file

--- idontgiveagame/Assets/Scripts/WordGame/WordGame.cs
+++ idontgiveagame/Assets/Scripts/WordGame/WordGame.cs
@@ -1,24 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using idgag.GameState;
 
-public class WordGame : MonoBehaviour
+namespace idgag.WordGame
 {
-    public struct FucksBucketMod {
-        int baseChange;
-        int modifier;
-        int fucksBucketKey;
-    }
-
-    // Start is called before the first frame update
-    void Start()
+    public class WordGame : MonoBehaviour
     {
-        
-    }
+        public struct FucksBucketMod
+        {
+            int baseChange;
+            int modifier;
+            FuckBucketTarget fucksBucketKey;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        List<FucksBucketMod> modifications;
+
+        // Start is called before the first frame update
+        void Start() {
+            foreach (FuckBucketTarget foo in
+                System.Enum.GetValues(typeof(FuckBucketTarget))) {
+
+
+            }
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
     }
 }
