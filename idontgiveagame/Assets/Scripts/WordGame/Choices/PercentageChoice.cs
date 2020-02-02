a899d050175b2f07c7bc2b10205f71697acf8ff3
Paul Mauviel <p.a.mauviel@gmail.com>
2020-02-02 12:56:24 -0500

    Working Percentage. Bug fix

--- idontgiveagame/Assets/Scripts/WordGame/Choices/PercentageChoice.cs
+++ idontgiveagame/Assets/Scripts/WordGame/Choices/PercentageChoice.cs
@@ -16,6 +16,29 @@ namespace idgag.WordGame {
 
             Int32.TryParse(max, out maximum);
 
+            initialize(minimum, maximum);
+
+        }
+
+        public PercentageChoice(string max) : base() {
+            int minimum = 0;
+            int maximum = 100;
+
+            Int32.TryParse(max, out maximum);
+
+            initialize(minimum, maximum);
+        }
+
+        public PercentageChoice() : base() {
+            int minimum = 0;
+            int maximum = 100;
+
+            initialize(minimum, maximum);
+        }
+
+        void initialize(int minimum, int maximum) {
+            operation = ChoiceOperation.ADD;
+
             maximum = Mathf.Clamp(maximum, 0, 100);
 
             int numberOfOptions = 10;
@@ -24,63 +47,13 @@ namespace idgag.WordGame {
                 Option newOption = new Option();
                 int label = minimum + (i * spreadPerOption);
 
-                newOption.value = label;
+                newOption.value = i / 2;
                 if (!options.ContainsKey(label.ToString())) {
                     options.Add(label.ToString(), newOption);
                 }
-            }
 
-            if (!options.ContainsKey(maximum.ToString())) {
-                Option newOption = new Option();
-                newOption.value = 5;
-
-                options.Add(maximum.ToString(), newOption);
-            }
-        }
-
-        public PercentageChoice(string max) : base() {
-            this.operation = ChoiceOperation.ADD;
-
-            int minimum = 0;
-            int maximum = 100;
-
-            Int32.TryParse(max, out maximum);
-
-            maximum = Mathf.Clamp(maximum, 0, 100);
-
-            for (int i = 1; i < maximum + 1; i++) {
-                Option newOption = new Option();
-                int label = 0 + (i);
-
-                newOption.value = label / 100 * 5;
-                if (!options.ContainsKey(label.ToString())) {
-                    options.Add(label.ToString(), newOption);
-                }
-            }
-
-            if (!options.ContainsKey(maximum.ToString())) {
-                Option newOption = new Option();
-                newOption.value = 5;
-
-                options.Add(maximum.ToString(), newOption);
-            }
-        }
-
-        public PercentageChoice() : base() {
-            operation = ChoiceOperation.ADD;
-
-            int minimum = 0;
-            int maximum = 100;
-
-            int numberOfOptions = 10;
-            int spreadPerOption = (maximum - minimum) / numberOfOptions;
-            for (int i = 0; i < numberOfOptions + 1; i++) {
-                Option newOption = new Option();
-                int label = 0 + (i * spreadPerOption);
-
-                newOption.value = label / 100 * 5;
-                if (!options.ContainsKey(label.ToString())) {
-                    options.Add(label.ToString(), newOption);
+                if (this.currentChoice == null) {
+                    this.currentChoice = newOption;
                 }
             }
 
@@ -92,5 +65,7 @@ namespace idgag.WordGame {
             }
         }
     }
+
+
 
 }
