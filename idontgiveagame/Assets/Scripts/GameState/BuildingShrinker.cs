36155c327948abad20aac74a63b364d00a9d8689
Dankrushen <henryclose@gmail.com>
2020-02-02 14:56:13 -0500

    Animate building sinking

--- idontgiveagame/Assets/Scripts/GameState/BuildingShrinker.cs
+++ idontgiveagame/Assets/Scripts/GameState/BuildingShrinker.cs
@@ -11,18 +11,49 @@ namespace idgag.GameState
         private Vector3 initialPosition;
         public float amountToSink = 190;
 
+        public float percentPerTime = 0.001f;
+
+        public float curPercent = 1;
+        public float targetPercent = 1;
+
         private void Awake()
         {
             initialPosition = transform.position;
         }
 
-        public void SinkByPercent(float percent)
+        private void FixedUpdate()
+        {
+            if (targetPercent == curPercent)
+                return;
+
+            float distToPercent = targetPercent - curPercent;
+
+            if (distToPercent <= percentPerTime)
+            {
+                curPercent = targetPercent;
+                DirectSinkByPercent(targetPercent);
+            }
+            else
+            {
+                curPercent += distToPercent < 0 ? -percentPerTime : percentPerTime;
+                DirectSinkByPercent(curPercent);
+            }
+        }
+
+        public void DirectSinkByPercent(float percent)
         {
             transform.position = initialPosition - new Vector3(0, amountToSink * percent, 0);
         }
 
+        public void SinkByPercent(float percent)
+        {
+            targetPercent = percent;
+        }
+
         public void ResetPosition()
         {
+            curPercent = 1;
+            targetPercent = 1;
             transform.position = initialPosition;
         }
     }
